6bca8667cd16f3ae55e6af53b401475ca2c267cb
Jose Silva <js38902@gmail.com>
2024-01-01 19:59:39 -0500

    added quieries

---
+++ seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO department (name) VALUES
+('Engineering')
+('TeamLead')
+('Operators')
+('Machinist')
+
+
+
+INSERT INTO role (title, salary, department_id) VALUES
+('Sales Lead, ')
+
+INSERT INTO 
