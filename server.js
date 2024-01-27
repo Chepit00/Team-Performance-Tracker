593d7496b5f9bad01b37a9e98069bd1176f54a39
Jose Silva <js38902@gmail.com>
2024-01-27 18:36:29 -0500

    comments

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const listInquirer = require("./lib/inquirer");
-const db = require('./db/db');
+const db = require("./db/db");
 
-db.connect(err => {
-    if (err)
-    console.error("Couldn't connect to Database.", err);
-    console.log('Connected to Tracker Database.');
-    listInquirer();
+db.connect((err) => {
+  if (err) console.error("Couldn't connect to Database.", err);
+  console.log("Connected to Tracker Database.");
+  listInquirer();
 });
-
-
